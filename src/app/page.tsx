d8eb2ce90dce2a6b2566d0e82cad15739929779c
Mou <m.mounir.f@gmail.com>
2024-03-14 03:49:44 +0200

    add subtle dotted grid background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<h1>Hi there</h1>
-			<ThemeSwitcher />
-		</main>
+		<div className="relative flex min-h-screen flex-col dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+			<main className={styles.main}>
+				<h1>Hi there</h1>
+				<ThemeSwitcher />
+			</main>
+		</div>
 	);
 }
