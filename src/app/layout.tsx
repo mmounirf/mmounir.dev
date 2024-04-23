f2c31724f82b8bdbaab1c3f957b898c74e1d08fe
Mou <m.mounir.f@gmail.com>
2024-04-23 19:33:15 +0200

    Refactor utils import paths and file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
