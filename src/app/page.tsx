f43bf5b9d174d2e20430885b841165cab3c92fae
Mou <m.mounir.f@gmail.com>
2024-04-23 19:33:32 +0200

    add BentoGrid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
 import { Footer, Header } from "@/components/ui";
+import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import { IconBraces, IconMessage, IconRoad, IconWriting } from "@tabler/icons-react";
+import Skills from "./components/Skills";
 
 export default function Home() {
 	return (
 		<div className="relative flex min-h-screen flex-col dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
 			<Header />
 			<main className="container py-8 flex flex-col flex-1 justify-center ">
-				<div className="flex flex-col gap-24">
+				<BentoGrid className="max-w-4xl mx-auto md:auto-rows-[15rem]">
+					<BentoGridItem
+						href="/skills"
+						title="Tech Arsenal"
+						description="Building exceptional experiences with a powerful tech stack."
+						header={<Skills />}
+						className="md:col-span-2"
+						icon={<IconBraces size={20} className="text-neutral-500" />}
+					/>
+					<BentoGridItem
+						href="/"
+						title="Get in touch!"
+						description="Connect for your next project or tech chat."
+						header={<h1>Header</h1>}
+						className="md:col-span-1"
+						icon={<IconMessage size={20} className="text-neutral-500" />}
+					/>
+
+					<BentoGridItem
+						href="/"
+						title="Technical Scribbles"
+						description="My software & product engineering insights."
+						header={<h1>Header</h1>}
+						className="md:col-span-1"
+						icon={<IconWriting size={20} className="text-neutral-500" />}
+					/>
+					<BentoGridItem
+						href="/"
+						title="Career Journey"
+						description="My path in the software development landscape."
+						header={<h1>Header</h1>}
+						className="md:col-span-2"
+						icon={<IconRoad size={20} className="text-neutral-500" />}
+					/>
+				</BentoGrid>
+				{/* <div className="flex flex-col gap-24">
 					<div>
 						<h2 className="flex gap-4 font-display text-9xl">
 							Hi there <div className="absolute right-10 sm:relative sm:right-0 motion-safe:animate-bounce">👋🏻</div>
@@ -18,7 +56,7 @@ export default function Home() {
 							and Egypt 🇪🇬.
 						</h2>
 					</div>
-				</div>
+				</div> */}
 			</main>
 			<Footer />
 		</div>
---
+++ src/components/ui/bento-grid.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { cn } from "@/utils";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export const BentoGrid = ({
+	className,
+	children,
+}: {
+	className?: string;
+	children?: React.ReactNode;
+}) => {
+	return (
+		<div className={cn("grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ", className)}>
+			{children}
+		</div>
+	);
+};
+
+export const BentoGridItem = ({
+	href,
+	className,
+	title,
+	description,
+	header,
+	icon,
+}: {
+	href: string;
+	className?: string;
+	title?: string | React.ReactNode;
+	description?: string | React.ReactNode;
+	header?: React.ReactNode;
+	icon?: React.ReactNode;
+}) => {
+	return (
+		<motion.div initial="initial" animate="animate" whileHover="hover" className={cn("row-span-1 w-full", className)}>
+			<Link
+				href={href}
+				className="row-span-1 h-full flex flex-col space-y-4 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border hover:border-transparent justify-between"
+			>
+				{header}
+				<div className="group-hover/bento:translate-x-2 transition duration-200">
+					{icon}
+					<div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">{title}</div>
+					<div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">{description}</div>
+				</div>
+			</Link>
+		</motion.div>
+	);
+};
