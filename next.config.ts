e465c1416e669bdcf6ed00795b193e85b451f7de
Mou <m.mounir.f@gmail.com>
2025-10-07 16:32:51 +0200

    migrate to react-router-7

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {};
-
-export default nextConfig;
-
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
